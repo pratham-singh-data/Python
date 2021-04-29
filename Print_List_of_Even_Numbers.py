9ad372b53ff15c414faad49ec7e3e43e55d11741
pratham-singh-data <83379036+pratham-singh-data@users.noreply.github.com>
2021-04-29 22:20:20 +0530

    New feature in Print_List_of_Even_Numbers.py
    I added the ability to choose which version of your program must be run.
    Otherwise after the first one running, he second would never run as exit() would end the execution.

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,17 +1,21 @@
-# Very sort method to creat list of even number form a given list
+# Very short method to create list of even number fromm a given list
 #Advance-Python
-list_number=list(map(int,input().split()))
-even_list=[i for i in list_number if i%2==0]
-print(even_list)
-exit()# Another one
-n = int(input("Enter the required range : "))  # user input
-list = []
+print("Method 1: Enter a list and a list containing only even numbers from that list is given as output")
+print("Method 2: Enter a range and a list of positive even numbers is returned 
+choice = int(input("Which method would yo like to apply(1 or 2) :"))
+valid_choices = [1, 2]
 
-if (n < 0):
-    print("Not a valid number, please enter a positive number!")
+if choice == 1:
+    list_number=list(map(int,input().split()))
+    even_list=[i for i in list_number if i%2==0]
+    print(even_list)
+elif choice == 2:
+    n = int(input("Enter the required range : "))  # user input
+    list = []
+
+    if (n < 0):
+        print("Not a valid number, please enter a positive number!")
+    else:
+        print(list(range(0,n+1, 2))) # stes are set to 2 so number returned is always even
 else:
-    for i in range(0,n+1):
-        if(i%2==0):
-            list.append(i)          #appending items to the initialised list getting from the 'if' statement
-
-print(list)
+    print('Invalid Choice')
